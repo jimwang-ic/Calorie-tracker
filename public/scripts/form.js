8245ecd83d416496f42a6382d2d0073b4fa2f47c
Cheng Lun Chen <SamChen@Chengs-MacBook-Pro.local>
2013-04-17 17:05:38 -0400

    form type 4

--- public/scripts/form.js
+++ public/scripts/form.js
@@ -8,21 +8,45 @@ window.addEventListener('load', function(){
 		
 }, false);
 
+var myHash = {}; // New object
+	myHash['Breakfast'] = [];
+	myHash['Lunch'] = [];
+	myHash['Dinner'] = [];
+	myHash['Snack'] = [];
+	myHash['Beverage'] = [];
+
 
 function saveFormValue (event) {
  	// alert("you clicked my link!");
  	var food = document.getElementById("foodentry").value;
- 	var type = document.getElementById("meal_type").value
- 	// split the string with the regular expression, need to check with user input in the future
- 	var foodArr = food.split(' ');
+ 	var type = document.getElementById("meal_type").value;
  	
- 	var typewrapper = '<td>'+type+'</td>';
- 	var foodwrapper = '<td>'+food+'</td>';
-	var tofill = document.createElement('tr');
- 	tofill.innerHTML = typewrapper + foodwrapper;
- 	var table_container = document.getElementById("table_container");
- 	table_container.appendChild(tofill);
- 	console.log(table_container);
- 	console.log(type);
- 	console.log(food);
+ 	
+ 	if(myHash.hasOwnProperty(type)){
+ 			myHash[type] = myHash[type] + food+" ";
+ 	}
+
+ 	// split the string with the regular expression, need to check with user input in the future
+ 	
+ 	var foodArr = food.split(' +');
+	var typehtml = document.getElementById(type);
+
+ 	if(food !==""){
+ 		if(typehtml == null){
+	 		var typewrapper = '<td id='+type+'>'+type+'</td>';
+	 		var foodwrapper = '<td id='+type+'_content>'+food+'</td>';
+			var tofill = document.createElement('tr');
+	 		tofill.innerHTML = typewrapper + foodwrapper;
+	 		var table_container = document.getElementById("table_container");
+	 		table_container.appendChild(tofill);
+	 	}else{
+	 		var toquery = type +"_content";
+	 		var foodcontenthtml = document.getElementById(toquery);
+	 		foodcontenthtml.innerHTML = myHash[type];
+	 	}
+ 	}
+
+ 	// console.log(table_container);
+ 	// console.log(type);
+ 	// console.log(food);
 }
