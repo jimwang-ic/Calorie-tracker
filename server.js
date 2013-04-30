2395548d5dcbdff67c77993402e59d47d88bffb6
Edward Grystar <egrystar@cs.brown.edu>
2013-04-30 19:07:22 -0400

    fixed db deletions, added mealtype db support

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var dnode = require('dnode');
 var anyDB = require('any-db');
 var engines = require('consolidate');
+var fs = require('fs');
 
 // Set varable for library call
 var app = express();
@@ -32,17 +33,22 @@ app.set('views', __dirname + '/view');  // tell Express where to find templates
 //likely migrate this way down the page
 //what db to use?
 var conn = anyDB.createConnection('sqlite3://database.db');
-conn.query('CREATE TABLE users (userid INTEGER PRIMARY KEY, username TEXT, password TEXT);') 
-    .on('end', function() {
-      console.log('Made table!');
-    });
-conn.query('CREATE TABLE calendar (id INTEGER PRIMARY KEY, datetime INTEGER, foodweight BINARY, mealname TEXT, totalcalories INTEGER, foodid INTEGER, weight INTEGER);') 
-  .on('end', function() {
-    console.log('Made table!');
-    
-    //UNCOMMENT FOR TEST POINTS
-    //test();
-  });
+fs.exists('database.db',function(exists) {
+    if (exists == false) {
+	conn.query('CREATE TABLE users (userid INTEGER PRIMARY KEY, username TEXT, password TEXT);') 
+	    .on('end', function() {
+	    console.log('Made table!');
+	    });
+	conn.query('CREATE TABLE calendar (id INTEGER PRIMARY KEY, datetime INTEGER, foodweight BINARY, mealname TEXT, totalcalories INTEGER, foodid INTEGER, weight INTEGER, mealtype TEXT);') 
+	.on('end', function() {
+	    console.log('Made table!');
+	    
+	    //UNCOMMENT FOR TEST POINTS
+	    //test();
+	});
+    }
+  
+});
 
 
 
@@ -138,8 +144,9 @@ app.post('/addmeal', function(req,res) {
 	console.log("here!");
 	var meal = JSON.parse(req.body.meal);
 	console.log(meal);
-	food = meal['food']
-	time = meal['date']
+	var food = meal['food']
+	var time = meal['date']
+	var mealtype = meal['mealtype'];
 	console.log(meal);
 	ids = "";
 	names = "";
@@ -153,7 +160,7 @@ app.post('/addmeal', function(req,res) {
 	names += food[food.length-1]['name'];
 	calories += food[food.length-1]['calories'];
 	//id,date,foodorweight,name,calories,id,weight
-	conn.query('INSERT INTO calendar VALUES ($1,$2,$3,$4,$5,$6,$7)', [null,meal['date'],1,names,calories,ids,0],function(error,result){
+	conn.query('INSERT INTO calendar VALUES ($1,$2,$3,$4,$5,$6,$7,$8)', [null,meal['date'],1,names,calories,ids,mealtype,0],function(error,result){
 		console.log(error);
 	});
 	console.log("after");
@@ -200,7 +207,7 @@ app.get('/graph.json',function(req,res) {
 	
 		    console.log('about to return');
 		    console.log(row);
-		    //examinePrevious();
+		    examinePrevious();
 		    res.json(data);
 	    })
 
@@ -233,6 +240,7 @@ app.get('/entry.json',function(req,res) {
 		    else {
 			entry['food'] = [row.foodid];
 		    }
+		    entry['mealtype'] = row.mealtype;
 		}
 		else {
 		    entry['weight'] = row.weight;
@@ -251,7 +259,7 @@ app.get('/entry.json',function(req,res) {
 
 CALENDAR TO DATABASE
 SENDS: month/year (mm-yy), username
-returns: rows of database: meal name, day, id
+returns: rows of database: meal name, day, id, totalcalories, mealtype
 
 **/
 app.get('/calendar.json',function(req,res) {
@@ -269,13 +277,14 @@ app.get('/calendar.json',function(req,res) {
     var name = req.username;
     console.log("start " + start);
     console.log(end);
-    conn.query('SELECT mealname,datetime,totalcalories,id FROM calendar WHERE datetime BETWEEN $1 AND $2',[start,end])
+    conn.query('SELECT mealname,datetime,totalcalories,id,mealtype FROM calendar WHERE datetime BETWEEN $1 AND $2',[start,end])
 	.on('row',function(row) {
 	    var day = new Date(row.datetime).getDate();
 	    var entry = {};
 	    entry['mealname'] = row.mealname;
 	    entry['id'] = row.id;
 	    entry['totalcalories'] = row.totalcalories;
+	    entry['mealtype'] = row.mealtype;
 	    data[day] = entry;
 	})
 	.on('end',function(row) {
@@ -346,15 +355,21 @@ function addUser(username,password) {
 function examinePrevious() {
     //first get max date from database
     console.log("THIS SHIT RIGHT HERE");
-    conn.query('SELECT mealname,datetime,id FROM calendar WHERE datetime BETWEEN $1 AND $2',[0,9999999999999],function(err,rows,fields) {
-	    console.log("LAST ENTRY: " + rows);
-	    console.log(fields);
-	    console.log(err);
-	});
+    var time;
+    conn.query('SELECT MAX(datetime) AS date from calendar',function(err,rows,fields) {
+	/*console.log(rows);
+	time = parseInt(rows['rows'][0]['date']);
+	console.log(new Date(time));
+	var today = new Date();
+	console.log(today);
     
-    conn.query('SELECT MAX(datetime) from calendar',function(err,rows,fields) {
-	console.log(rows);
-	});
+	var difference = today.getTime() - time;
+	var hoursdiff = difference / 216000;
+	console.log(hoursdiff/24 + " days and " + hoursdiff % 24 + " hours");*/
+    });
+
+    
+    
     
 }
 
